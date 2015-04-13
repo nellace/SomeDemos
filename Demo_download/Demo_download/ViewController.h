30a6ea778343c75b8753be657534aab7c63b9fc7
nellace <nellace@qq.com>
2015-04-13 18:00:26 +0800

    create new demo

---
+++ Demo_download/Demo_download/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo_download
+//
+//  Created by WO on 15-4-13.
+//  Copyright (c) 2015年 sg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demo_download/Demo_downloadTests/Demo_downloadTests.m
@@ -0,0 +1,40 @@
+//
+//  Demo_downloadTests.m
+//  Demo_downloadTests
+//
+//  Created by WO on 15-4-13.
+//  Copyright (c) 2015年 sg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Demo_downloadTests : XCTestCase
+
+@end
+
+@implementation Demo_downloadTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

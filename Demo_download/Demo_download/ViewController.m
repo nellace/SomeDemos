b779a05db050b275b9e30927d297773077f48ee7
nellace <nellace@qq.com>
2015-04-13 18:45:00 +0800

    create new download

--- Demo_download/Demo_download/ViewController.m
+++ Demo_download/Demo_download/ViewController.m
@@ -8,8 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
 
+@interface ViewController ()
+{
+    UIProgressView *pro;
+}
 @end
 
 @implementation ViewController
@@ -17,7 +20,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    btn.frame = CGRectMake(240, 40, 60, 40);
+    pro.backgroundColor = [UIColor blackColor];
+    [btn setTitle:@"开始下载" forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(downLoad) forControlEvents:UIControlEventTouchUpInside];
+    pro = [[UIProgressView alloc]initWithFrame:CGRectMake(10, 60, 200, 20)];
+    pro.backgroundColor = [UIColor blackColor];
+    [self.view addSubview:pro];
+    [self.view addSubview:btn];
 }
+
+//AFHTTPSession下载功能
+- (void)downLoad
+{
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc]initWithSessionConfiguration:configuration];
+    NSURL *url = [NSURL URLWithString:@"http://ghost.25pp.com/soft/pp_mac.dmg"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    NSURLSessionDownloadTask *downloadTask = [manager downloadTaskWithRequest:request progress:nil destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {
+        NSURL *documentsDirectectoryUrl = [[NSFileManager defaultManager] URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil];
+        NSLog(@"%f",pro.progress);
+        return [documentsDirectectoryUrl URLByAppendingPathComponent:[response suggestedFilename]];
+    } completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
+        NSLog(@"File downloaded to: %@",filePath);
+        [[[UIAlertView alloc]initWithTitle:@"提示" message:@"下载完成" delegate:self cancelButtonTitle:@"queding" otherButtonTitles: nil]show];
+    }];
+    [pro setProgressWithDownloadProgressOfTask:downloadTask animated:YES];
+    [downloadTask resume];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
